33317e3c89561d106121b9cef6a4956e44001ae8
AyeletNissan <ayelet.nissan@post.runi.ac.il>
2023-12-27 16:06:43 +0200

    Add code for hw 2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,10 +2,10 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int num = Integer.parseInt(args[0]);
 		for(int i=0; i<num; i++){
-			for(int j=0; j<num; j++){
-				if(i%2!=0 && j==0){
+			if(i%2!=0){
 					System.out.print(" ");
-				}
+			}
+			for(int j=0; j<num; j++){
 					System.out.print("* ");
 			}
 			System.out.println();
