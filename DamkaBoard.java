aa6ad1490d8b61d84f883b527ab83fca5fd25d09
AyeletNissan <ayelet.nissan@post.runi.ac.il>
2023-12-27 18:56:44 +0200

    add

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,18 +2,17 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for(int i=1; i<=n; i++){
-			if(i%2==0){
-				System.out.print(" ");
-			}
 			for(int j=1; j<=n; j++){
-				if (j==n && i%2==0) {
-					System.out.print("*");
-				} else{
+				if(i%2==0){
+				    System.out.print(" *");
+			    }else{
 					System.out.print("* ");
 				}
 			}
-				System.out.println();
-		}
+			System.out.println();
+		}	
 		System.out.println();
+		}
+		
 	}
-}
+
